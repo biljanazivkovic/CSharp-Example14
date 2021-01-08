a53ee2a3c9db5857a9332ed1f68a9f411ccdabaa
Biljana Zivkovic <77129646+biljanazivkovic@users.noreply.github.com>
2021-01-08 16:25:20 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex14
+{
+    class Car
+    {
+        private string model = "Mustang";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ex14
+{
+    class Program
+    {
+        //private string model = "Mustang";
+
+        static void Main(string[] args)
+        {
+            Car Ford = new Car();
+            Console.WriteLine(Ford.model);
+        }
+    }
+}
